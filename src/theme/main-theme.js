0e99832ffd999e4c4d92ecaf1e7d48dd87fffe10
石渡拓海 <t-ishiwata@sight-visit.com>
2021-03-30 03:02:25 +0900

    [Add] themeの追加

---
+++ src/theme/main-theme.js
@@ -0,0 +1,17 @@
+export const customTheme = {
+  colors: {
+    primary: '#EDB268',
+    secondary: '#A4D0DF',
+    third: '#177197',
+    white: '#ffffff',
+  },
+  fonts: {
+    header: '"Helvetica Neue", Helvetica, Arial, sans-serif',
+  },
+  fontSizes: {
+    h1: '72px',
+    h2: '64px',
+    slideTitle: '50px',
+  },
+  space: [16, 24, 32, 240],
+};
